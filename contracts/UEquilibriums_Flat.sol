622d4fc1a4eca8179739f4fa883ecbcba6dc2378
AndyPuppyZ <44590470+AndyPuppyZ@users.noreply.github.com>
2020-01-30 00:56:33 +0200

    adding contract

--- contracts/UEquilibriums_Flat.sol
+++ contracts/UEquilibriums_Flat.sol
@@ -426,7 +426,334 @@ library SafeMathInt {
 
 
 
+/**
+ * @title Median Oracle
+ *
+ * @notice Provides a value onchain that's aggregated from a whitelisted set of
+ *         providers.
+ */
+contract MedianOracle is Ownable, IOracle {
+    using SafeMath for uint256;
 
+    struct Report {
+        uint256 timestamp;
+        uint256 payload;
+        
+    }
+
+    // Addresses of providers authorized to push reports.
+    address[] public providers;
+
+    // Addresses of the main providers.
+    address[] public mainProviders;
+
+    // Reports indexed by provider address. Report[0].timestamp > 0
+    // indicates provider existence.
+    mapping (address => Report[2]) public providerReports;
+
+    event ProviderAdded(address provider);
+    event ProviderRemoved(address provider);
+    event ReportTimestampOutOfRange(address provider);
+    event ProviderReportPushed(address indexed provider, uint256 payload, uint256 timestamp);
+
+    // The number of seconds after which the report is deemed expired.
+    uint256 public reportExpirationTimeSec;
+
+    // The number of seconds since reporting that has to pass before a report
+    // is usable. /// Time between reports
+    uint256 public reportDelaySec;
+
+    // The minimum number of providers with valid reports to consider the
+    // aggregate report valid.
+    uint256 public minimumProviders = 1;
+
+    // Timestamp of 1 is used to mark uninitialized and invalidated data.
+    // This is needed so that timestamp of 1 is always considered expired.
+    uint256 private constant MAX_REPORT_EXPIRATION_TIME = 520 weeks;
+
+    /**
+    * @param reportExpirationTimeSec_ The number of seconds after which the
+    *                                 report is deemed expired.
+    * @param reportDelaySec_ The number of seconds since reporting that has to
+    *                        pass before a report is usable
+    * @param minimumProviders_ The minimum number of providers with valid
+    *                          reports to consider the aggregate report valid.
+    */
+    constructor(uint256 reportExpirationTimeSec_,
+                uint256 reportDelaySec_,
+                uint256 minimumProviders_)
+        public
+    {
+        require(reportExpirationTimeSec_ <= MAX_REPORT_EXPIRATION_TIME);
+        require(minimumProviders_ > 0);
+        reportExpirationTimeSec = reportExpirationTimeSec_;
+        reportDelaySec = reportDelaySec_;
+        minimumProviders = minimumProviders_;
+    }
+
+     /**
+     * @notice Sets the report expiration period.
+     * @param reportExpirationTimeSec_ The number of seconds after which the
+     *        report is deemed expired.
+     */
+    function setReportExpirationTimeSec(uint256 reportExpirationTimeSec_)
+        external
+        onlyOwner
+    {
+        require(reportExpirationTimeSec_ <= MAX_REPORT_EXPIRATION_TIME);
+        reportExpirationTimeSec = reportExpirationTimeSec_;
+    }
+
+    /**
+    * @notice Sets the time period since reporting that has to pass before a
+    *         report is usable.
+    * @param reportDelaySec_ The new delay period in seconds.
+    */
+    function setReportDelaySec(uint256 reportDelaySec_)
+        external
+        onlyOwner
+    {
+        reportDelaySec = reportDelaySec_;
+    }
+
+    /**
+    * @notice Sets the minimum number of providers with valid reports to
+    *         consider the aggregate report valid.
+    * @param minimumProviders_ The new minimum number of providers.
+    */
+    function setMinimumProviders(uint256 minimumProviders_)
+        external
+        onlyOwner
+    {
+        require(minimumProviders_ > 0);
+        minimumProviders = minimumProviders_;
+    }
+
+    /**
+     * @notice Pushes a report for the calling provider.
+     * @param payload is expected to be 18 decimal fixed point number.
+     */
+    
+    function pushReport(uint256 payload) external
+    {
+      
+
+        address providerAddress = msg.sender;
+        Report[2] storage reports = providerReports[providerAddress];
+        uint256[2] memory timestamps = [reports[0].timestamp, reports[1].timestamp];
+
+        require(timestamps[0] > 0);
+
+        uint8 index_recent = timestamps[0] >= timestamps[1] ? 0 : 1;
+        uint8 index_past = 1 - index_recent;
+
+        // Check that the push is not too soon after the last one.
+        require(timestamps[index_recent].add(reportDelaySec) <= now);
+
+        reports[index_past].timestamp = now;
+        reports[index_past].payload = payload;
+        
+
+        emit ProviderReportPushed(providerAddress, payload, now);
+    }
+
+    /**
+    * @notice Invalidates the reports of the calling provider.
+    */
+    function purgeReports() external
+    {
+        address providerAddress = msg.sender;
+        require (providerReports[providerAddress][0].timestamp > 0);
+        providerReports[providerAddress][0].timestamp=1;
+        providerReports[providerAddress][1].timestamp=1;
+    }
+
+    /**
+    * @notice Computes median of provider reports whose timestamps are in the
+    *         valid timestamp range.
+    * @return AggregatedValue: Median of providers reported values.
+    *         valid: Boolean indicating an aggregated value was computed successfully.
+    */
+    
+            address MainAddress;
+            uint256 public Where = 0;
+        uint256 public index = 0;
+        uint256 public mainCount =0;
+         uint256 public regularNodes;
+        uint256  public size ;
+        address public nodeAddress;
+        uint256 public nodeIndex;
+        uint256[]  public  validReports;
+
+    function getData()
+        external
+        returns (uint256, bool)
+
+    {  size=0;
+        MainAddress=address(0);
+        regularNodes=0;
+        validReports.length = 0;
+        nodeAddress= address(0);
+        nodeIndex=0;
+        mainCount =0;
+        index=0;
+        Where = 0;
+        uint256   reportsCount = providers.length;
+        
+        uint256 minValidTimestamp =  now.sub(reportExpirationTimeSec);
+        uint256 maxValidTimestamp =  now.sub(reportDelaySec);
+
+        for (uint256 i = 0; i < reportsCount; i++) {
+            address providerAddress = providers[i];
+            Report[2] memory reports = providerReports[providerAddress];
+
+            uint8 index_recent = reports[0].timestamp >= reports[1].timestamp ? 0 : 1;
+            uint8 index_past = 1 - index_recent;
+            uint256 reportTimestampRecent = reports[index_recent].timestamp;
+            if (reportTimestampRecent > maxValidTimestamp) {
+                // Recent report is too recent.
+                Where++;
+                uint256 reportTimestampPast = providerReports[providerAddress][index_past].timestamp;
+                if (reportTimestampPast < minValidTimestamp) {
+                    // Past report is too old.
+                    Where=2;
+                    emit ReportTimestampOutOfRange(providerAddress);
+                } else if (reportTimestampPast > maxValidTimestamp) {
+                    // Past report is too recent.
+                    Where=3;
+                    emit ReportTimestampOutOfRange(providerAddress);
+                } else { Where = 4;
+                    // Using past report.
+                    validReports.push(providerReports[providerAddress][index_past].payload);
+                    size++;
+                    for (uint256 j = 0; j < mainProviders.length; j++) {
+                        if(mainProviders[j] == providerAddress){
+                        MainAddress  = mainProviders[j];
+                        index = index_past;
+                        mainCount++;
+                        }
+                        if(mainProviders[j] != providerAddress){
+                         nodeAddress  = providerAddress;
+                         nodeIndex = index_past;
+                        }
+                        
+                    }
+                    
+                }
+            } else { Where=5;
+                // Recent report is not too recent.
+                if (reportTimestampRecent < minValidTimestamp) { Where=6;
+                    // Recent report is too old.
+                    emit ReportTimestampOutOfRange(providerAddress);
+                } else {Where=7;
+                    // Using recent report.
+                    validReports.push(providerReports[providerAddress][index_recent].payload);
+                    size++;
+                    for (uint256 j = 0; j < mainProviders.length; j++) {
+                        if(mainProviders[j] == providerAddress){
+                        MainAddress  = mainProviders[j];
+                        index = index_recent;
+                        mainCount++;
+                        }
+                        if(mainProviders[j] != providerAddress){
+                         nodeAddress  = providerAddress;
+                         nodeIndex = index_recent;
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        if (size < minimumProviders) {
+            return (0, false);
+        }
+
+         regularNodes = validReports.length - mainCount;
+        if(regularNodes == 0 || mainCount == 0 )
+        {
+          return (0, false);
+        }
+         if((regularNodes - 1) == mainCount){
+         return (Select.computeMedian(validReports, size), true);
+
+         }
+        if(regularNodes != mainCount){
+
+         while((regularNodes - 1) > mainCount){
+        validReports.push(providerReports[mainProviders[0]][index].payload);
+        size++;
+        mainCount++;
+
+        }
+
+        while((regularNodes - 1) < mainCount){
+        validReports.push(providerReports[nodeAddress][nodeIndex].payload);
+        size++;
+        regularNodes++;
+
+        }
+        return (Select.computeMedian(validReports, size), true);
+        }
+
+        validReports.push(providerReports[nodeAddress][nodeIndex].payload);
+        size++;
+        return (Select.computeMedian(validReports, size), true);
+    }
+
+    /**
+     * @notice Authorizes a provider.
+     * @param provider Address of the provider.
+     */
+    function addProvider(address provider)
+        external
+        onlyOwner
+    {
+        require(providerReports[provider][0].timestamp == 0);
+        providers.push(provider);
+        providerReports[provider][0].timestamp = 1;
+        emit ProviderAdded(provider);
+    }
+    function addMainProvider(address provider)
+        external
+        onlyOwner
+    {
+        mainProviders.push(provider);
+        emit ProviderAdded(provider);
+    }
+
+    /**
+     * @notice Revokes provider authorization.
+     * @param provider Address of the provider.
+     */
+    function removeProvider(address provider)
+        external
+        onlyOwner
+    {
+        delete providerReports[provider];
+        for (uint256 i = 0; i < providers.length; i++) {
+            if (providers[i] == provider) {
+                if (i + 1  != providers.length) {
+                    providers[i] = providers[providers.length-1];
+                }
+                providers.length--;
+                emit ProviderRemoved(provider);
+                break;
+            }
+        }
+    }
+
+    /**
+     * @return The number of authorized providers.
+     */
+    function providersSize()
+        external
+        view
+        returns (uint256)
+    {
+        return providers.length;
+    }
+}
 
 
 
@@ -578,6 +905,8 @@ contract Equilibrium is ERC20Detailed, Ownable {
         require(to != address(this));
         _;
     }
+
+    MedianOracle public MedianO;
     
     uint256 private constant DECIMALS = 9;
     uint256 public nodePrice = 50000 * 10**DECIMALS;
